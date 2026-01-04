13aebb6c736b7051da88e255faf0ed872446d804
KevZ3742 <kzhang3742@gmail.com>
2026-01-04 15:20:24 -0600

    new question

---
+++ 158B_Taxi.py
@@ -0,0 +1,26 @@
+# https://codeforces.com/problemset/problem/158/B
+# rating: 1100
+
+from collections import Counter
+
+count = int(input())
+
+groups = Counter(map(int, input().split()))
+
+cars = 0
+
+cars += groups[4]
+
+cars += groups[3]
+groups[1] = max(0, groups[1] - groups[3])
+
+cars += groups[2] // 2
+groups[2] %= 2
+
+if groups[2] == 1:
+    cars += 1
+    groups[1] = max(0, groups[1] - 2)
+
+cars += (groups[1] + 3) // 4
+
+print(cars)
