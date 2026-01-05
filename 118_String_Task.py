6d149c78288020c867173c835bc0ba45cd50ee83
KevZ3742 <kzhang3742@gmail.com>
2026-01-05 09:51:38 -0600

    2 more solves

---
+++ 118_String_Task.py
@@ -0,0 +1,21 @@
+# https://codeforces.com/problemset/problem/118/A
+# rating: 1000
+
+string = str(input())
+
+string = string.lower()
+
+vowels = "aeiouy"
+for char in vowels:
+    if char in string:
+
+        string = string.replace(char, "")
+
+string = list(string)
+
+counter = 0
+for i in range(len(string)):
+    string.insert(i+counter, ".")
+    counter += 1
+
+print(''.join(string))
--- 160A_Twins.py
+++ 160A_Twins.py
@@ -1,3 +1,20 @@
 # https://codeforces.com/problemset/problem/160/A
 # rating: 900
 
+count = int(input())
+coins = list(map(int, input().split()))
+
+coins.sort(reverse=True)
+
+mySum = 0
+myCoinCount = 0
+total = sum(coins)
+
+for n in coins:
+    mySum += n
+    total -= n
+    myCoinCount += 1
+    if mySum > total:
+        break
+
+print(myCoinCount)
