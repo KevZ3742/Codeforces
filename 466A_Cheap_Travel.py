17edfa701173bee26dd7199344d849767507c37f
KevZ3742 <kzhang3742@gmail.com>
2026-01-06 09:30:12 -0600

    solves

---
+++ 466A_Cheap_Travel.py
@@ -0,0 +1,23 @@
+# https://codeforces.com/problemset/problem/466/A
+# rating: 1200
+
+input = list(map(int, input().split()))
+
+rides = input[0]
+ticket = input[1]
+rideCost = input[2]
+ticketCost = input[3]
+
+ticketRatio = ticket / ticketCost
+money = 0
+
+while ticketCost < rides * rideCost and ticketRatio > 1 / rideCost and rides > 0:
+    money += ticketCost
+    rides -= ticket
+
+if rides < 0:
+    rides = 0
+
+money += rides * rideCost
+
+print(money)
