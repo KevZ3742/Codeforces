780c8fc592d7d22fa6639c02dcda1e0a8f6629b1
KevZ3742 <kzhang3742@gmail.com>
2026-01-09 09:09:10 -0600

    solves

--- 2183C_War_Strategy.py
+++ 2183C_War_Strategy.py
@@ -6,11 +6,21 @@ testCases = int(input())
 for i in range(testCases):
     n, m, k = map(int, input().split())
 
-    l = k - 1
-    r = n - k
+    l = 0
+    r = 0
 
-    bases = 0
+    while True:
+        tried = False
 
-    print(bases)
+        if l < k - 1 and l + max(l + 1, r) + r <= m:
+            l += 1
+            tried = True
+        
+        if r < n - k and l + max(l, r + 1) + r <= m:
+            r += 1
+            tried = True
+        
+        if not tried:
+            break
 
-# wip
+    print(l + r + 1)
---
+++ 514A_Chewbacca_And_Number.py
@@ -0,0 +1,17 @@
+# https://codeforces.com/problemset/problem/514/A
+# rating: 1200
+
+num = str(input())
+
+toPrint = ""
+
+
+for i in range(len(num)):
+    if i == 0 and num[i] == "9":
+        toPrint += "9"
+    elif int(num[i]) < 9 - int(num[i]):
+        toPrint += num[i]
+    else:
+        toPrint += str(9 - int(num[i]))
+
+print(int(toPrint))
