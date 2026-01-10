bb6f7c9f040e17a874c6f2c29c49821508100ebc
KevZ3742 <kzhang3742@gmail.com>
2026-01-10 11:51:53 -0600

    cleaned up

---
+++ 1520D_Same_Differences.py
@@ -0,0 +1,17 @@
+# https://codeforces.com/problemset/problem/1520/D
+# rating: 1200
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    counter = 0
+    mapp = {}
+    for i in range(n):
+        key = a[i] - i
+        counter += mapp.get(key, 0)
+        mapp[key] = mapp.get(key, 0) + 1
+
+    print(counter)
