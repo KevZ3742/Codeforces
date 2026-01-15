5248de737c17c36c58640850913f66e65c1bd22b
KevZ3742 <kzhang3742@gmail.com>
2026-01-15 08:50:22 -0600

    solve

---
+++ 427A_Police_Recruits.py
@@ -0,0 +1,18 @@
+# http://codeforces.com/contest/427/problem/A
+# rating: 800
+
+n = int(input())
+a = list(map(int, input().split()))
+
+cops = 0
+crimes = 0
+for i in a:
+    if i > 0:
+        cops += i
+    else:
+        if cops == 0:
+            crimes += 1
+        else:
+            cops -= 1
+
+print(crimes)
