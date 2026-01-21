97b4795669fd4f0b35dec85bdcafd73fbd8d2770
KevZ3742 <kzhang3742@gmail.com>
2026-01-20 20:49:20 -0600

    solve

---
+++ 443A_Anton_And_Letters.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/contest/443/problem/A
+# rating: 800
+
+letters = str(input())
+letters = letters[1:-1]
+letters = set(map(str.strip, letters.split(',')))
+
+if letters == {''}:
+    print(0)
+else:
+    print(len(letters))
