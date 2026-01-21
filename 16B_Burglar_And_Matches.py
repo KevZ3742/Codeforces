a13c9929a8a56fa96e491c746155d9c533a3de61
KevZ3742 <kzhang3742@gmail.com>
2026-01-21 16:40:33 -0600

    solves

---
+++ 16B_Burglar_And_Matches.py
@@ -0,0 +1,24 @@
+# https://codeforces.com/contest/16/problem/B
+# rating: 900
+
+from collections import defaultdict
+
+n, m = map(int, input().split())
+
+matches = defaultdict(int)
+for _ in range(m):
+    a, b = map(int, input().split())
+    matches[b] += a
+
+sortedMatches = sorted(matches.keys(), reverse=True)
+
+toPrint = 0
+for box in sortedMatches:
+    if n == 0:
+        break
+
+    boxesToTake = min(n, matches[box])
+    toPrint += boxesToTake * box
+    n -= boxesToTake
+
+print(toPrint)
---
+++ 686A_Free_Ice_Cream.py
@@ -0,0 +1,19 @@
+# https://codeforces.com/contest/686/problem/A
+# rating: 800
+
+n, x = map(int, input().split())
+
+distress = 0
+for _ in range(n):
+    operation, d = map(str, input().split())
+    d = int(d)
+
+    if operation == "-":
+        if x >= d:
+            x -= d
+        else:
+            distress += 1
+    else:
+        x += d
+
+print(x, distress)
