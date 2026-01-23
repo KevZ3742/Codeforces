5820f86e6ddaa9edf459980474bfd2d783cb562d
KevZ3742 <kzhang3742@gmail.com>
2026-01-23 17:11:13 -0600

    solve

---
+++ 136A_Presents.py
@@ -0,0 +1,12 @@
+# https://codeforces.com/contest/136/problem/A
+# rating: 800
+
+n = int(input())
+p = list(map(int, input().split()))
+
+toPrint = [0] * n
+
+for i in range(n):
+    toPrint[p[i] - 1] = i + 1
+
+print(*toPrint)
