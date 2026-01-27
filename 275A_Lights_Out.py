d6b7401526da6fac907c3664094ed97cccb665e0
KevZ3742 <kzhang3742@gmail.com>
2026-01-27 14:24:27 -0600

    solve

---
+++ 275A_Lights_Out.py
@@ -0,0 +1,23 @@
+# https://codeforces.com/problemset/problem/275/A
+# rating: 900
+
+switches = []
+
+for _ in range(3):
+    row = list(map(int, input().split()))
+    switches.append(row)
+
+lights = [[1 for _ in range(3)] for _ in range(3)]
+
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
+
+for r in range(len(switches)):
+    for c in range(len(switches[r])):
+        if switches[r][c] % 2 != 0:
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < 3 and 0 <= nc < 3:
+                    lights[nr][nc] ^= 1
+
+for row in lights:
+    print("".join(map(str, row)))
---
+++ 32B_Borze.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/32/B
+# rating: 800
+
+s = str(input())
+
+digits = [".", "-.", "--"]
+number = ""
+
+currDigit = ""
+for i in s:
+    currDigit += i
+
+    for j in range(len(digits)):
+        if digits[j] == currDigit:
+            number += str(j)
+            currDigit = ""
+
+print(number)
