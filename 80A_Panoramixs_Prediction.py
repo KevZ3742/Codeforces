4cbdab81ad313d4ee02dfc9ac9b6c0e3ea70179d
KevZ3742 <kzhang3742@gmail.com>
2026-01-28 10:10:27 -0600

    solve

--- 80A_Panoramixs_Prediction.py
+++ 80A_Panoramixs_Prediction.py
@@ -3,5 +3,17 @@
 
 n, m = map(int, input().split())
 
-# wip
+def isPrime(num):
+    for i in range(2, int(num**.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
+nextNum = n + 1
+while not isPrime(nextNum):
+    nextNum += 1
+
+if nextNum == m:
+    print("YES")
+else:
+    print("NO")
