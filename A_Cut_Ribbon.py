e7d7b93c7a39c6b8989e1dd2e214ee5c1b219ffe
KevZ3742 <kzhang3742@gmail.com>
2026-02-15 22:26:19 -0600

    solve

---
+++ A_Cut_Ribbon.py
@@ -0,0 +1,19 @@
+# rating: 1300
+
+n, a, b, c = map(int, input().split())
+
+dp = [-1] * (n + 1)
+dp[0] = 0
+
+for i in range(min(a, b, c), n + 1):
+    if i >= a:
+        dp[i] = max(dp[i], dp[i - a] + 1)
+    if i >= b:
+        dp[i] = max(dp[i], dp[i - b] + 1)
+    if i >= c:
+        dp[i] = max(dp[i], dp[i - c] + 1)
+
+    if dp[i] == 0:
+        dp[i] = -1
+
+print(dp[n])
---
+++ A_Rank_List.py
@@ -0,0 +1,12 @@
+# rating: 1100
+
+n, k = map(int, input().split())
+
+teams = []
+for _ in range(n):
+    p, t = map(int, input().split())
+    teams.append((-p, t))
+
+teams.sort()
+
+print(teams.count(teams[k - 1]))
