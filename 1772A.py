185d32189881eea1ecf422615e6cc8b55a11a9c0
KevZ3742 <kzhang3742@gmail.com>
2026-03-15 18:30:09 -0500

    solve

---
+++ 1772A.py
@@ -0,0 +1,12 @@
+# A. A+B?
+# https://codeforces.com/problemset/problem/1772/A
+# rating: 800
+
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split("+"))
+
+    print(a + b)
+
+    
