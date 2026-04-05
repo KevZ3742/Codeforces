8ac219952ea48f375d9ee3378e89b4336f42d0c6
KevZ3742 <kzhang3742@gmail.com>
2026-04-05 10:04:12 -0500

    solve

--- 1847B.py
+++ 1847B.py
@@ -16,13 +16,12 @@ for _ in range(t):
         print(1)
     else:
         toPrint = 0
+        f = -1
 
-        f = a[0]
-        for num in a[1:]:
+        for num in a:
             f &= num
-
             if f == 0:
                 toPrint += 1
-                f = num
+                f = -1
 
         print(toPrint)
