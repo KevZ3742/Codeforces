e2f9b779e45e1cbefed881028d904840151e1e08
KevZ3742 <kzhang3742@gmail.com>
2026-04-09 02:13:02 -0500

    solve

--- 2183C.py
+++ 2183C.py
@@ -2,25 +2,24 @@
 # https://codeforces.com/contest/2183/problem/C
 # rating: 1500
 
-testCases = int(input())
+t = int(input())
 
-for i in range(testCases):
+for _ in range(t):
     n, m, k = map(int, input().split())
 
     l = 0
     r = 0
-
     while True:
         tried = False
 
-        if l < k - 1 and l + max(l + 1, r) + r <= m:
+        if l < k - 1 and l + r + max(l + 1, r) <= m:
             l += 1
             tried = True
-        
-        if r < n - k and l + max(l, r + 1) + r <= m:
+
+        if r < n - k and l + r + max(l, r + 1) <= m:
             r += 1
             tried = True
-        
+
         if not tried:
             break
 
