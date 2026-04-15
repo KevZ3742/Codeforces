2644a1ff34f537f71fb64e76b86728e64d6d481a
KevZ3742 <kzhang3742@gmail.com>
2026-04-15 15:23:40 -0500

    solve

--- 1913B.py
+++ 1913B.py
@@ -8,22 +8,15 @@ t = int(input())
 
 for _ in range(t):
     s = input()
-    
+
     sCounter = Counter(s)
 
-    if sCounter["0"] == sCounter["1"]:
-        print(0)
-        continue
-    
-    used = len(s)
-    for i in range(len(s)):
-        if s[i] == "1" and sCounter["0"]:
+    for c in s:
+        if c == "1" and sCounter["0"]:
             sCounter["0"] -= 1
-            used -= 1
-        elif s[i] == "0" and sCounter["1"]:
+        elif c == "0" and sCounter["1"]:
             sCounter["1"] -= 1
-            used -= 1
         else:
             break
 
-    print(used)
+    print(sCounter["0"] + sCounter["1"])
