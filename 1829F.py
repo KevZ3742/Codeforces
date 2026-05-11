2ffae5a391d8c73ff849b5a5892eecd1a46ebc1c
KevZ3742 <kzhang3742@gmail.com>
2026-05-11 05:53:41 -0500

    solve

--- 1829F.py
+++ 1829F.py
@@ -2,12 +2,25 @@
 # https://codeforces.com/problemset/problem/1829/F
 # rating: 1300
 
+from collections import Counter
+
 t = int(input())
 
 for _ in range(t):
     n, m = map(int, input().split())
 
+    edges = []
+
     for i in range(m):
         u, v = map(int, input().split())
+        edges.append(u)
+        edges.append(v)
 
-        # wip
+    edgeCounter = Counter(edges)
+    valueCounter = Counter(edgeCounter.values())
+    sortedValueCounter = valueCounter.most_common()
+
+    if sortedValueCounter[-1][1] != 1:
+        print(sortedValueCounter[-1][0], sortedValueCounter[-1][0] - 1)
+    else:
+        print(sortedValueCounter[-1][0], sortedValueCounter[-2][0] - 1)
--- 1916C.py
+++ 1916C.py
@@ -2,11 +2,34 @@
 # https://codeforces.com/problemset/problem/1916/C
 # rating: 1200
 
+def pattern(n):
+    group = n // 3
+    position = n % 3
+
+    if position == 1:
+        return group + 1
+    
+    return group
+
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
 
-    # wip
-    
+    oddCount = 0
+    curSum = 0
+    toPrint = []
+    for i in range(n):
+        curSum += a[i]
+
+        if a[i] % 2:
+            oddCount += 1
+
+        if i == 0:
+            toPrint.append(curSum)
+            continue
+
+        toPrint.append(curSum - pattern(oddCount))
+
+    print(*toPrint)
